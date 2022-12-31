487c5e1f5f05d696699dff833eee9425f917bb12
ben <benmotykax@gmail.com>
2022-12-31 14:34:13 +0100

    changed template to t3 turbo

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: [
+      "./tsconfig.json",
+      "./apps/*/tsconfig.json",
+      "./packages/*/tsconfig.json",
+    ],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended"],
+  reportUnusedDisableDirectives: true,
+};
