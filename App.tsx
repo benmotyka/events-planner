7e74c7a18e7a26d8aafb998c115be9789e379641
ben <benmotykax@gmail.com>
2023-01-05 22:51:44 +0100

    added maps module

--- App.tsx
+++ App.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 import "./styles";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
 
+const latitude = 50.0412;
+const longitude = 21.9991;
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        initialRegion={{
+          latitude,
+          longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          identifier="test"
+          coordinate={{ latitude, longitude }}
+          draggable={false}
+          // image={{ uri: "custom_pin" }}
+          title="test"
+        />
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
